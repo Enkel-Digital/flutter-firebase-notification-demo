5baeccce5a3f66ad39642305104e105f9acd0e03
Jaime <jaimeloeuf@gmail.com>
2022-04-27 14:52:51 +0800

    Add settings page.

---
+++ app/lib/settings.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+
+// import './data.dart';
+
+class Settings {
+  // int? lastSync;
+}
+
+class SettingsWidget extends StatelessWidget {
+  final Settings settings = Settings();
+  SettingsWidget({Key? key}) : super(key: key);
+
+  // final Settings settings;
+  // const SettingsWidget({Key? key, required this.settings}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            // @todo Add: Version number + date + build time
+            // @todo Add: Show last sync time
+            // @todo Add: Check for update and ask user to update if outdated
+
+            // Button to open feedback form
+            Link(
+              target: LinkTarget.self,
+              uri: Uri.parse("https://www.google.com"), // @todo
+              builder: (context, followLink) => ElevatedButton(
+                onPressed: followLink,
+                child: const Text("Feedback Form"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
